53a4c3e80a757efdf427ff2b4b9317c6797ca8fd
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2023-03-15 18:44:02 -0300

    I've learnt basic C++ data types.

--- hello.cpp
+++ hello.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 int main()
 {
-    string characterFirstName = "John";
-    string characterLastName = "Doe";
-    int characterAge = 80;
+    /*
+      * Some data types in C++:
+      *
+      * 1. char - Single-character text.
+      * 2. string - Multi-character text.
+      * 3. int - Integer numbers.
+      * 4. float - Floating-point numbers.
+      * 5. double - An "extension" for the float type (most used).
+    */
 
-    cout << "There once was a man named " << characterFirstName << " " << characterLastName << "..." << endl;
-    cout << "He was " << characterAge << " years old..." << endl;
-    cout << "He liked the name 'Doe'..." << endl;
-    cout << "But did not like being " << characterAge << "..." << endl;
+    char firstLetterOfMyName = 'L';
+    string myName = "Louis L.";
+    int myAge = 16; // MAY NOT use floating point with integers!
+    double myHeight = 1.7;
+
+    cout << "Hi! The first letter of my name is '" << firstLetterOfMyName << "' (because my name is '" << myName << "'). I am " << myAge << ", and my height is " << myHeight << "m :D" << endl;
 
     return 0;
 }
