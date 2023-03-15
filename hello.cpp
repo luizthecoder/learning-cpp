e7f3f8a915b9497fb6285cf57cb8d11cc2022b64
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2023-03-15 18:48:50 -0300

    Boolean data added to last file.

--- hello.cpp
+++ hello.cpp
@@ -11,12 +11,14 @@ int main()
       * 3. int - Integer numbers.
       * 4. float - Floating-point numbers.
       * 5. double - An "extension" for the float type (most used).
+      * 6. bool - Boolean values.
     */
 
     char firstLetterOfMyName = 'L';
     string myName = "Louis L.";
     int myAge = 16; // MAY NOT use floating point with integers!
     double myHeight = 1.7;
+    bool isTall = true;
 
     cout << "Hi! The first letter of my name is '" << firstLetterOfMyName << "' (because my name is '" << myName << "'). I am " << myAge << ", and my height is " << myHeight << "m :D" << endl;
 
