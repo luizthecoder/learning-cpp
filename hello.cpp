08fab11d5f068455853999df6260c479d16c0145
Gustavo Oliveira <gustavooliveira120406@gmail.com>
2023-03-15 17:56:32 -0300

    First commit. | "Hello, World!" printed out.

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+/* Hello, C++! */
+#include <iostream> // Imports "iostream" library.
+using namespace std; // Makes standard variables be able to be used.
+
+int main() // Function executed when the C++ program runs. All the code's gotta be inside of it.
+{
+    cout << "Hello, World!"; // "cout" stands for "console out", and it's used together with the "<<" operator to print something out (in this example, "Hello, World!").
+    return 0;
+}
